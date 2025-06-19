b7f545a79345960823e3ff6a85ec1625bf5e4219
sakir-shaikh <sakir.shaikh@brevitaz.com>
2025-06-19 12:32:43 +0530

    extracted logic for the generating row info.

--- src/components/AllRowInfo.jsx
+++ src/components/AllRowInfo.jsx
@@ -1,18 +1,21 @@
 import generateDivisibilityLabel from "../utils/generateDivisibilityLabel";
+
+function generateAllRowInfo(rows) {
+  return rows.map((row) => {
+    const divisibleBy = generateDivisibilityLabel({ item: row });
+    return (
+      <li key={row.id}>
+        {row.name} counter : value is: {row.counterValue}, it is divisible by{" "}
+        {divisibleBy}
+      </li>
+    );
+  });
+}
+
 function AllRowInfo({ rows }) {
   return (
     <div className="all-counter-info-container">
-      <ul>
-        {rows.map((row) => {
-          const divisibleBy = generateDivisibilityLabel({ item: row });
-          return (
-            <li key={row.id}>
-              {row.name} counter : value is: {row.counterValue}, it is divisible
-              by {divisibleBy}
-            </li>
-          );
-        })}
-      </ul>
+      <ul>{generateAllRowInfo(rows)}</ul>
     </div>
   );
 }
