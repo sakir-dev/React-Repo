9db4b4eab59c34d5ca639cb2bcba91f9c3ae8013
sakir-shaikh <sakir.shaikh@brevitaz.com>
2025-06-20 16:04:54 +0530

    Implemented the List component for the allrowinfo

--- src/components/AllRowInfo.jsx
+++ src/components/AllRowInfo.jsx
@@ -1,22 +1,44 @@
+import { ListItem, ListIcon, UnorderedList, Box } from "@chakra-ui/react";
+import { InfoIcon, WarningIcon } from "@chakra-ui/icons";
 import generateDivisibilityLabel from "../utils/generateDivisibilityLabel";
 
 function generateAllRowInfo(rows) {
   return rows.map((row) => {
     const divisibleBy = generateDivisibilityLabel({ item: row });
     return (
-      <li key={row.id}>
+      <ListItem key={row.id}>
+        <ListIcon as={InfoIcon} color="green.500" />
         {row.name} counter : value is: {row.counterValue}, it is divisible by{" "}
         {divisibleBy}
-      </li>
+      </ListItem>
     );
   });
 }
 
 function AllRowInfo({ rows }) {
   return (
-    <div className="all-counter-info-container">
-      <ul>{generateAllRowInfo(rows)}</ul>
-    </div>
+    <Box
+      p={6}
+      bg="gray.50"
+      border="1px solid"
+      borderColor="gray.200"
+      rounded="lg"
+      boxShadow="md"
+      maxW="container.md"
+      mx="auto"
+      mt={6}
+      color={"black"}
+    >
+      <UnorderedList
+        spacing={4}
+        pl={6}
+        color="gray.800"
+        fontSize="md"
+        fontWeight="medium"
+      >
+        {generateAllRowInfo(rows)}
+      </UnorderedList>
+    </Box>
   );
 }
 
